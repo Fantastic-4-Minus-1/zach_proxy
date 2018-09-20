a5e7aef1019f83d27dc5df52948702ac8840ca31
Roy Lee <roylee@Andrews-MacBook-Pro.local>
2018-09-19 20:17:13 -0700

    initial commit

---
+++ stock_fluctuation/client/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+// window.stockFluctuation = App;
+ReactDOM.render(<App />, document.getElementById('stockFluctuation'));
